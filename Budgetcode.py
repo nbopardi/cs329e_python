e05bf28a44da337521c9d31518ed73d99ffd4636
BradyHughes <33072250+BradyHughes@users.noreply.github.com>
2018-10-07 16:14:15 -0500

    Add files via upload

---
+++ Budgetcode.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import unittest
+import numpy
+
+df = pd.read_csv('budget.csv')
+
+print(df)
+
+x = df.describe()
+
+#print(x)
+
+y = df["Date"]
+
+#print(y)
+
+price=None
+category=None
+date=None
+
+def newexpense():
+    global price
+    global category
+    global date
+    global df
+    price = input("Enter Price")
+    category = input("Enter Type of Expense")
+
+    date = input("Enter Date")
+    f = open("budget.csv", "a")
+    f.write('\n')
+    
+
+    df2 = pd.DataFrame(columns=['Date', 'type', 'Ammount'])
+    df2.loc[0] = [date, category, price]
+
+    df2.to_csv(f, index=False, header=False)
+
+    f.close
+    df3 = pd.read_csv('budget.csv')
+
+    return df3
+    
+
+class sumunittest(unittest.TestCase):
+
+    def test_summing(self):
+        self.assertEqual(sumitems(df),805.5)
+    def test_newexpense(self):
+        global price
+        global date
+        global category
+        global df
+        self.assertNotEqual(price, None)
+        self.assertNotEqual(category, None)
+        self.assertNotEqual(date, None)
+        self.assertIn(category, str(df["type"]))
+        self.assertIn(price, str(df["Ammount"]))
+        self.assertIn(date, str(df["Date"]))
+        
+
+def sumitems(df):
+    z = df["Ammount"]
+    total = 0.0 
+    for items in z:
+        length = len(items)
+        sObject = slice(1,length)
+        numerical = items[sObject]
+        total += float(numerical)
+    
+    return(total)
+
+
+
+def main():
+    global df
+    
+    y = newexpense()
+    x = sumitems(y)
+    print("Sum:", x)
+    
+    
+
+    
+main()
+
+unittest.main()
