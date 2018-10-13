29191ce224cf9be2f1b21e9a373355d532b92e17
hamzsait <hamzasait@utexas.edu>
2018-10-13 10:49:41 -0500

    Update Budgetcode.py

--- Budgetcode.py
+++ Budgetcode.py
@@ -40,24 +40,7 @@ def newexpense():
     df3 = pd.read_csv('budget.csv')
 
     return df3
-    
-
-class sumunittest(unittest.TestCase):
-
-    def test_summing(self):
-        self.assertEqual(sumitems(df),805.5)
-    def test_newexpense(self):
-        global price
-        global date
-        global category
-        global df
-        self.assertNotEqual(price, None)
-        self.assertNotEqual(category, None)
-        self.assertNotEqual(date, None)
-        self.assertIn(category, str(df["type"]))
-        self.assertIn(price, str(df["Ammount"]))
-        self.assertIn(date, str(df["Date"]))
-        
+       
 
 def sumitems(df):
     z = df["Ammount"]
@@ -83,5 +66,3 @@ def main():
 
     
 main()
-
-unittest.main()
