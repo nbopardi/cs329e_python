dd08036cb3c521d2e4ad8c04d333417a53e367e2
hamzsait <hamzasait@utexas.edu>
2018-10-13 10:48:34 -0500

    Update test_Budgetcode.py

--- test_Budgetcode.py
+++ test_Budgetcode.py
@@ -5,3 +5,14 @@ class sumunittest(unittest.TestCase):
 
     def test_summing(self):
         self.assertEqual(sumitems(),805.5)
+    def test_newexpense(self):
+        global price
+        global date
+        global category
+        global df
+        self.assertNotEqual(price, None)
+        self.assertNotEqual(category, None)
+        self.assertNotEqual(date, None)
+        self.assertIn(category, str(df["type"]))
+        self.assertIn(price, str(df["Ammount"]))
+        self.assertIn(date, str(df["Date"]))
