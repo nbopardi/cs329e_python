277d4f070ace36e48f013b7b8f9d2bf70cd8221e
Nikhil Bopardikar <nikhilb@gmail.com>
2018-10-21 18:56:56 -0500

    Added gui functionality for modify entry

--- Project_1/gui.py
+++ Project_1/gui.py
@@ -27,7 +27,7 @@ class Application(tk.Frame):
     def createWidgets(self):
         self.modifyButton = tk.Button(self)
         self.modifyButton["text"] = "Modify Entry"
-        self.modifyButton["command"] = self.say_hi
+        self.modifyButton["command"] = self.modifyRowEntry
         self.modifyButton.pack(side="top")
 
         self.addButton = tk.Button(self)
@@ -88,6 +88,62 @@ class Application(tk.Frame):
 
     def addExpenseToDf(self, date, category, amount):
         self.df = newexpense(df=self.df, amount=amount, category=category, date=date)
+        self.table.redraw()
+        self.table.sortTable(0) # Sort by column index 0, which is date
+
+    def modifyRowEntry(self):
+
+        top = tk.Toplevel()
+        top.title("Modify Row Entry")
+
+        rowLabel = tk.Label(top, text='Row to Modify', font=('Times', 12))
+        dateLabel = tk.Label(top, text='Date (Optional)', font=('Times', 12))
+        typeLabel = tk.Label(top, text='Category (Optional)', font=('Times', 12))
+        amountLabel = tk.Label(top, text='Amount (Optional)', font=('Times', 12))
+
+        rowLabel.grid(row=0,column=0)
+        dateLabel.grid(row=1,column=0)
+        typeLabel.grid(row=2,column=0)
+        amountLabel.grid(row=3,column=0)
+
+        rowEntry = tk.Entry(top)
+        dateEntry = tk.Entry(top)
+        typeEntry = tk.Entry(top)
+        amountEntry = tk.Entry(top)
+
+        rowEntry.grid(row=0, column=1)
+        dateEntry.grid(row=1,column=1)
+        typeEntry.grid(row=2,column=1)
+        amountEntry.grid(row=3,column=1)
+
+        addButton = tk.Button(top,text="Modify Expense",
+                    command=lambda:[self.modifyExpenseToDf(rowEntry.get(),
+                                    dateEntry.get(),
+                                    typeEntry.get(),
+                                    amountEntry.get()),
+                                    top.destroy()])
+
+        addButton.grid(row=4,column=0)
+
+    def modifyExpenseToDf(self, row, date, category, amount):
+        try:
+            row = int(row)
+            row = row - 1 # Reduce row value by 1 to match with index value
+        except: 
+            print("Please enter an integer for the row value")
+            return
+
+        if row < 0 or row >= self.df.shape[0]:
+            print("Please enter a valid integer value")
+            return
+
+        if date == "":
+            date = None
+        if category == "":
+            category = None
+        if amount == "":
+            amount = None
+        self.df = modifyEntry(df=self.df, row=row, date=date, category=category, amount=amount)
         self.table.redraw()
         self.table.sortTable(0) # Sort by column index 0, which is date
 
