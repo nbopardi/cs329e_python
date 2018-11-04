ed7246c8c6d2e13f79c04e43144ef1a6dd85313a
hamzsait <hamzasait@utexas.edu>
2018-11-04 14:55:34 -0600

    Update QuizShow_test.py

--- Project_2/QuizShow_test.py
+++ Project_2/QuizShow_test.py
@@ -1,12 +1,35 @@
 import unittest
 from QuizShow import *
+from Player import *
 
 class TestQuizShow(unittest.TestCase):
 
 
-    def testObjects(self):
+    def testQuestionObjects(self):
         question1 = Question("Monkey.jpg", "What does his face tell you?", ["Happy", "Sad", "Angry", "Nervous"],"Happy")
         self.assertEqual(question1.image, "Monkey.jpg")
         self.assertEqual(question1.question,"What does his face tell you?")
         self.assertEqual(question1.options, ["Happy", "Sad", "Angry", "Nervous"])
         self.assertEqual(question1.answer, "Happy")
+
+    def testPlayerObjects(self):
+        player1 = Player("Turkel")
+        self.assertEqual(player1.playerName,"Turkel")
+        self.assertEqual(player1.pointValue,0)
+
+    def testaddPoints(self):
+        player2 = Player("Purkel")
+
+        player2.addPoints(2)
+        self.assertEqual(player2.pointValue,2)
+
+        player2.addPoints(-3)
+        self.assertEqual(player2.pointValue,0)
+
+    def testgetPointValue(self):
+        player3 = Player("Jurkel")
+
+        self.assertEqual(player3.getPointValue(),0)
+
+        player3.addPoints(5)
+        self.assertEqual(player3.getPointValue(),5)
