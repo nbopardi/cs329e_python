cbc5dde080eb806d8b20fa4b70ac16af28f9736f
Nikhil Bopardikar <nikhil@wireless-10-147-26-234.public.utexas.edu>
2018-11-08 11:49:31 -0600

    Updating player class tlass to include judge flag and storing latest caption

--- Project_2/Player.py
+++ Project_2/Player.py
@@ -9,11 +9,14 @@
 
 class Player():
 
-	def __init__(self, name):
+	def __init__(self, name, isJudge=False, caption = ''):
 
 		self.playerName = name
 		self.pointValue = 0
+		self.isJudge = isJudge
+		self.caption = caption
 
+	# Add (or subtract) points to the player's total
 	def addPoints(self, pointsEarned):
 
 		self.pointValue = self.pointValue + pointsEarned
@@ -21,10 +24,20 @@ class Player():
 		if self.pointValue < 0:
 			self.pointValue = 0
 
+	# Gets the player's point value
 	def getPointValue(self):
 
 		return self.pointValue
 
+	# Gets the player's name
 	def getName(self):
 		
 		return self.playerName
+
+	# Set if the player is the judge or not for the round (True or False)
+	def setJudge(self, isJudgeForRound):
+		self.isJudge = isJudgeForRound
+
+	# Store the user's latest answer for the "Caption-this" mode
+	def storeAnswer(self, answer):
+		self.caption = answer
